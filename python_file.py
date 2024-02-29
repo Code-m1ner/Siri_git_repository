d4abe1ed9989edd93a2eff5e010887a173ee0224
Rashid <Rashid.JumaMwinyi@horsa.it>
2024-02-29 14:24:51 +0100

    made the loop into a function

--- python_file.py
+++ python_file.py
@@ -1,9 +1,14 @@
 print("this is not easy, basiccaly a simple comand to execute this script is to run code and the file name 'code <file_name>' ")
 
-for x in range(2, 30, 3):
-    if x % 2 == 0:
-        print("Hello Dummy")
-    else:
-        print("Morto")    
+
+def function_1():
+    for i in range(2, 30, 3):
+        if i % 2 == 0:
+            print("Hello Dummy", i)
+        else:
+            print("Morto", i)    
+    return i
+
+function_1()
 
 print("testing the master branch")
