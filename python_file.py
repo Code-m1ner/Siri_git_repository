b9a3d9bf0873533c129c3e0d71a835bf86ddadcf
Rashid <Rashid.JumaMwinyi@horsa.it>
2024-02-29 12:06:44 +0100

    a simple loop in py file

--- python_file.py
+++ python_file.py
@@ -1,1 +1,7 @@
 print("this is not easy, basiccaly a simple comand to execute this script is to run code and the file name 'code <file_name>' ")
+
+for x in range(2, 30, 3):
+    if x % 2 == 0:
+        print("Hello Dummy")
+    else:
+        print("Morto")    
