d1451c5e2e15c53c534d8b4b529250515547bf34
Rashid <Rashid.JumaMwinyi@horsa.it>
2024-03-01 16:38:26 +0100

    debuging

--- myenv/automate/manage.py
+++ myenv/automate/manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+sys.setrecursionlimit(10**6)  # Set the recursion limit to a higher value
 
 def main():
     """Run administrative tasks."""
